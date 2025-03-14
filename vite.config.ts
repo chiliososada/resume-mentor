738dedc3bef53ff7f9af7d344d20ce136e0ec243
Liuziyuan <chiliososada@gmail.com>
2025-03-14 18:35:11 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,25 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(),
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -8,6 +30,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5002',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, '')  // 如果后端API路径不包含'/api'前缀则需要此行
+      }
+    }
   },
   plugins: [
     react(),
